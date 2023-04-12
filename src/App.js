f720329d5cba6764067594650f42b6163b1aa11d
Juanchi <jcesarotti.dev@gmail.com>
2023-04-11 22:00:44 -0300

    New test of Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Banner from "./components/Banner";
 import WhatiDo from "./components/WhatiDo";
+import MyNavbar from "./components/NavBar";
 import bannerImage from "./images/test_banner_image.png";
 
 function App() {
   return (
     <div>
+      <MyNavbar></MyNavbar>
       <Banner
         title="Juan Cesarotti"
         subtitle="FullStack Developer & Scrum "
